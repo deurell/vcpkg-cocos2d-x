4f9f11dfaecb564ccb8c9852c6811a15d3dd82c9
Dale Stammen <dalestam@microsoft.com>
2017-02-06 15:56:18 -0800

    don't delete zlibstatic.lib

--- ports/zlib/portfile.cmake
+++ ports/zlib/portfile.cmake
@@ -19,7 +19,7 @@ vcpkg_install_cmake()
 
 # Both dynamic and static are built, so keep only the one needed
 if(VCPKG_LIBRARY_LINKAGE STREQUAL dynamic)
-    file(REMOVE ${CURRENT_PACKAGES_DIR}/lib/zlibstatic.lib ${CURRENT_PACKAGES_DIR}/debug/lib/zlibstaticd.lib)
+    #file(REMOVE ${CURRENT_PACKAGES_DIR}/lib/zlibstatic.lib ${CURRENT_PACKAGES_DIR}/debug/lib/zlibstaticd.lib)
 else()
     file(REMOVE ${CURRENT_PACKAGES_DIR}/lib/zlib.lib ${CURRENT_PACKAGES_DIR}/debug/lib/zlibd.lib)
     file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/bin ${CURRENT_PACKAGES_DIR}/debug/bin)
